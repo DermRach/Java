63f34fb3ac6968af7f3d2858b7848d0756ca0ebf
DermRach <contato@junqueiraconsultoria.net.br>
2026-08-21 11:23:04 -0300

    Create LessonMethodStaticArgs.java

---
+++ Basico/LessonMethodStaticArgs.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// 11:22 21/08/2026
+// Sem IDE específica
+// Método estático sem retorno com argumentos
+// Versão 1.0
+// Autor: DermRach (Rafael)
+// Website: mundopauta.com.br
+
+public class LessonMethodStaticArgs
+{
+	static void showSoma(int valueA, int valueB)
+	{
+		System.out.println(valueA + valueB);
+	}
+
+	public static void main (String[]args)
+	{
+		showSoma(10,20);
+	}
+}
