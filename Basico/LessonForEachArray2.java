d33c5ccd29d25e44e108a801f1c0829eab3d5193
DermRach <contato@junqueiraconsultoria.net.br>
2026-08-21 12:05:02 -0300

    Add LessonForEachArray2 to demonstrate foreach usage

---
+++ Basico/LessonForEachArray2.java
@@ -0,0 +1,19 @@
+// 12:04 21/08/2026
+// Sem IDE específica
+// Uso do foreach para pecorrer um array [vetor] unidimensional e imprimir
+// Versão 1.0
+// Autor: DermRach (Rafael)
+// Website: mundopauta.com.br
+
+
+public class LessonForEachArray2
+{
+	static String[] nomes = {"SNES","N64","NES","MEGA DRIVE"};
+	public static void main (String[]args)
+	{
+		for (String n : nomes)
+		{
+			System.out.println(n);
+		}
+	}
+}
