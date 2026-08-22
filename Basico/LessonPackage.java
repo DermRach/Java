a25c92d4b347798a62c0ddd4fd6a31ef7059afcf
DermRach <contato@junqueiraconsultoria.net.br>
2026-08-22 01:36:00 -0300

    Add LessonPackage class to demonstrate ExamplePackage

---
+++ Basico/LessonPackage.java
@@ -0,0 +1,20 @@
+import org.example.ExamplePackage;
+
+// Código fonte ExamplePackage.java disponível neste repositório Java\Básico
+
+// 01:29 22/08/2026
+// Sem IDE específica
+// Uso do pacote ExamplePackage
+// Versão 1.0
+// Autor: DermRach (Rafael)
+// Website: mundopauta.com.br
+
+public class LessonPackage
+{
+	public static void main (String[]args)
+	{
+		ExamplePackage ep = new ExamplePackage();
+		ep.show();
+	}
+}
+
