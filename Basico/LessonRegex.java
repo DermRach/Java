b1e5ea0a7bd56587b5ed1a42db3e915d38efa870
DermRach <contato@junqueiraconsultoria.net.br>
2026-08-28 08:02:15 -0300

    Rename class from Lesson to LessonRegex

---
+++ Basico/LessonRegex.java
@@ -0,0 +1,26 @@
+import com.dermrach.PatternSearch;
+
+// 07:57 28/08/2026
+// Sem IDE específica
+// Program Pasta "Packages" com o nome PatternSearch
+// Utiliza Expressões Regulares 
+// Versão 1.0
+// Autor: DermRach (Rafael)
+// Website: mundopauta.com.br
+
+// A classe possui dois construtores.
+// Um que pode ser definido pelo usuário
+// E outro que tem uma pré-definição de palavra a ser encontrada
+
+
+public class LessonRegex
+{
+	public static void main (String[]args)
+	{
+		// Constructor 1
+		PatternSearch psObj1 = new PatternSearch("Emma");
+
+		// Constructor 2
+		PatternSearch psObj2 = new PatternSearch();
+	}
+}
